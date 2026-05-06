09575bf905cbb00a6348d3a4683dfa17819f822e
Subhajit Kar <subhajitkar19@gmail.com>
2026-05-06 14:37:10 +0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 using Logging
 
+include("project_deps.jl")
+
 # Existing tests
 include("boundary_conditions.jl")
 include("chebyshev.jl")
@@ -10,6 +12,7 @@ include("triglobal.jl")
 
 # v2.0 API tests
 include("test_types.jl")
+include("type_stability.jl")
 include("test_validation.jl")
 include("test_show.jl")
 include("test_problems.jl")
